2dfb5c3ae6e241ce7a0909bcf73d380576a5dc7d
Fran Hrzenjak <fran@changeset.hr>
2014-08-16 00:57:40 +0200

    pypi config

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Format timedelta values nicely in your templates.
 
 [![Build Status](https://api.travis-ci.org/frnhr/django_timedeltatemplatefilter.png)](https://travis-ci.org/frnhr/django_timedeltatemplatefilter)
 
+v0.1.1
+
+
 ## What
 
 New filter named "timedelta"
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django_timedeltatemplatefilter",
+    version = "0.1.1",
+    author = "Fran Hrzenjak",
+    author_email = "frnhr@changeset.hr",
+    description = ("Format timedelta values nicely in your Django templates."),
+    license = "The Unlicense",
+    keywords = "django timedelta template filter",
+    url = "http://packages.python.org/django_timedeltatemplatefilter",
+    packages=['timedeltatemplatefilter', ],
+    #long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: Utilities",
+        "Framework :: Django",
+        "License :: Public Domain",
+    ],
+)
